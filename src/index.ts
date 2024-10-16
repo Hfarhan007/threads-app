f323d938e7a87015a080ecef2044297860f2c48b
Farhan <h.farhan4362@gmail.com>
2024-10-16 11:30:54 +0500

    code Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,36 @@
 import express from "express";
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import http from 'http';
+import { expressMiddleware } from "@apollo/server/express4";
+import createApolloGraphqlServer from "./graphql";
+import UserService from "./services/user";
 
 async function init() {
-    const app = express();
-    const httpServer = http.createServer(app); // Create HTTP server instance
+  const app = express();
+  const PORT = Number(process.env.PORT) || 4000;
 
-    const PORT = Number(process.env.PORT) || 4000;
+  app.use(express.json());
 
-    app.use(express.json());
+  app.get("/", (req, res) => {
+    res.json({ message: "Server is up and running" });
+  });
 
-    const gqlServer = new ApolloServer({
-        typeDefs: `
-            type Query {
-                hello: String
-                say(name: String): String
-            }
-        `,
-        resolvers: {
-            Query: {
-                hello: () => `Hey there, I am a graphql server`,
-                say: (_, { name }: {name:string}) => `Hey ${name}, How are you?` // Added missing backtick and fixed destructuring
-            }
+  app.use(
+    "/graphql",
+    expressMiddleware(await createApolloGraphqlServer(), {
+      context: async ({ req }) => {
+        // @ts-ignore
+        const token = req.headers["token"];
+
+        try {
+          const user = UserService.decodeJWTToken(token as string);
+          return { user };
+        } catch (error) {
+          return {};
         }
-    });
+      },
+    })
+  );
 
-    await gqlServer.start();
-
-    app.get('/', (req, res) => {
-        res.json({ message: 'Server is up and running' });
-    });
-
-    // Apply middleware for GraphQL
-    app.use('/graphql', expressMiddleware(gqlServer));
-
-    httpServer.listen(PORT, () => {
-        console.log(`Server started at PORT: ${PORT}`);
-    });
+  app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
 }
 
-init();
+init();  
